d2e84789759611a8d76b79a521b5dd9d8fd5412a
TreTrenoff <85366443+TreTrenoff@users.noreply.github.com>
2025-09-18 09:06:54 +0200

    Motoquest

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.init()
 # Paramètres de la fenêtre
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Template Pygame")
+pygame.display.set_caption("MotoQuest")
 
 
 
